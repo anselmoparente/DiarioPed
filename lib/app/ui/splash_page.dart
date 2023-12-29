d9d5cb8de7a70808902df1e56b77231414136955
Anselmo Parente Martins <anselmoparente@gmail.com>
2023-12-29 14:32:48 -0300

    Finish splash screen

--- lib/app/ui/splash_page.dart
+++ lib/app/ui/splash_page.dart
@@ -5,6 +5,30 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[50],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Nutriped',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.indigo[900],
+              ),
+            ),
+            Image.asset(
+              'assets/images/splash_icon.png',
+              height: size.width * 0.9,
+              width: size.width * 0.9,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nutriped/app/ui/splash_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashPage(),
+      },
     );
   }
 }
